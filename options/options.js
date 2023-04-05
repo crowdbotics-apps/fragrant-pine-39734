1fea71c481bef5e4e5b6f63ee17f89d7b41f6762
Crowdbotics <team@crowdbotics.com>
2023-04-05 21:45:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-pine-39734",
+  url: "https://fragrant-pine-39734.botics.co",
+  api: "https://fragrant-pine-39734.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
